fd21e6e05b42919364d2f41b44f8c28e5bc94ac2
Chres <clc8680@rit.edu>
2019-06-27 21:12:21 -0400

    played around with combining graphs

--- pds_db.py
+++ pds_db.py
@@ -42,7 +42,7 @@ def get_all_pokemon_column(column):
     return data_list
 
 
-def get_all_pokemon_height_weight_graph():
+def get_all_pokemon_height_weight_graph(unit="metric"):
     get_all_pokemon_query = "SELECT weight_kg, height_m, name FROM pds WHERE weight_kg IS NOT NULL"
 
     cursor.execute(get_all_pokemon_query)
@@ -50,14 +50,30 @@ def get_all_pokemon_height_weight_graph():
     weight_list = []
     height_list = []
     name_list = []
-    for item in data:
-        weight_list.append(item[0])
-        height_list.append(item[1])
-        name_list.append(item[2])
+    if unit == "imperial":
+        for item in data:
+            weight_list.append(kg_to_lbs(item[0]))
+            height_list.append(m_to_ft(item[1]))
+            name_list.append(item[2])
+    else:
+        for item in data:
+            weight_list.append(item[0])
+            height_list.append(item[1])
+            name_list.append(item[2])
 
     data_list = [weight_list, height_list, name_list]
     print(data_list)
     return data_list
+
+
+def kg_to_lbs(kg):
+    lbs = round(kg * 2.20462, 2)
+    return lbs
+
+
+def m_to_ft(m):
+    ft = round(m * 3.28084, 2)
+    return ft
 
 
 if __name__ == '__main__':
--- visualize.py
+++ visualize.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import pds_db as pds
 import mplcursors
 
-weights = pds.get_all_pokemon_height_weight_graph()[0]
-heights = pds.get_all_pokemon_height_weight_graph()[1]
-names = pds.get_all_pokemon_height_weight_graph()[2]
+imperial_result = pds.get_all_pokemon_height_weight_graph("imperial")
+result = pds.get_all_pokemon_height_weight_graph()
+imperial_weights = imperial_result[0]
+imperial_heights = imperial_result[1]
+weights = result[0]
+heights = result[1]
+names = result[2]
 
-fig, ax = plt.subplots()
+fig = plt.figure(
+)
+ax = fig.add_subplot(121)
 ax.scatter(weights, heights)
 ax.set_title("Pokemon Weight(kg) vs Height(m)")
 
@@ -17,4 +23,17 @@ mplcursors.cursor(ax, hover=True).connect(
 
 plt.xlabel("Weight (kg)")
 plt.ylabel("Height (m)")
+
+ax = fig.add_subplot(122)
+ax.scatter(imperial_weights, imperial_heights)
+ax.set_title("Pokemon Weight(lbs) vs Height(ft)")
+
+mplcursors.cursor(ax, hover=True).connect(
+    "add", lambda sel: sel.annotation.set_text(
+        names[sel.target.index] + " " + str(imperial_weights[sel.target.index]) + " lbs, " + str(
+            imperial_heights[sel.target.index]) + " ft"))
+
+plt.xlabel("Weight (lbs)")
+plt.ylabel("Height (ft)")
+plt.tight_layout()
 plt.show()
